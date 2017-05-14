c1fcdb83cc038df196f28b10f90f7dab2a16ecd7
DESKTOP-867UHHI\alice <alice90270@gmail.com>
2017-05-15 01:42:06 +0800

    Encryption

--- 11217 - Encryption.c
+++ 11217 - Encryption.c
@@ -1,87 +1,124 @@
 #include<stdio.h>
 
 int main(){
-    char input[10000];
+    char input[10000]={};
+
     while(gets(input)){
         int len=0;
         int n=1;
         int i, j, k;
         len=strlen(input);
-        printf("len=%d\n", len);
+        //printf("len=%d\n", len);
 
         while(len>(n*n)){
             n++;
-            printf("n=%d\n", n);
+            //printf("n=%d\n", n);
         }
-        char table[n][n];
-        char tableTraced[n+1][n+1];
+        char table[50][50];
+        int tableTraced[50][50];
 
-        for(i=0;i<n;i++){
-            for(j=0;j<n;j++){
-                tableTraced[i][j]=0;
+        // initial
+        for(i=0;i<=n+1;i++){
+            for(j=0;j<=n+1;j++){
+                table[i][j]=' ';
+                //printf("[%c]", table[i][j]);
+                if(i==0||j==0||i==n+1||j==n+1)
+                    tableTraced[i][j]=1;
+                else
+                    tableTraced[i][j]=0;
             }
+            //printf("\n");
         }
+
         int index=0;
-        int row=0;
-        int col=0;
-        char now="down";
+        int row=1;
+        int col=1;
+        int down=0;
+        int right=1;
+        int up=2;
+        int left=3;
+        int now=0;
         //¤U ¥k ¤W ¥ª
-        while(index<len){
-            if(now="down"){
-                printf(input[index]);
-                table[row][col]=input[index];
-                tableTraced[row][col]=1;
-                index++;
-                if(row==n-1 || tableTraced[row+1][col]!=0){
-                    col++; // move right
-                    now="right";
-                }
-                else row++;      //down
+/*        for(i=0;i<=n+1;i++){
+            for(j=0;j<=n+1;j++){
+                    printf("[%d]", tableTraced[i][j]);
+
             }
-            else if(now="right"){
-                printf(input[index]);
-                table[row][col]=input[index];
-                tableTraced[row][col]=1;
-                index++;
-                if(col==n-1 || tableTraced[row][col+1]!=0){
+            printf("\n");
+     }  */
+        while(index<len){
+            //printf("%c\n", input[index]);
+            table[row][col]=input[index];
+            tableTraced[row][col]=1;
+            index++;
+
+            if(now%4==down){
+                if(tableTraced[row+1][col]!=0){
+                    col++; // move right
+                    now++;
+                }
+                else row++;//down
+            }
+            else if(now%4==right){
+                if(tableTraced[row][col+1]!=0){
                     row--; // move up
-                    now="up";
+                    now++;
                 }
                 else col++;
             }
-            else if(now="up"){
-                printf(input[index]);
-                table[row][col]=input[index];
-                tableTraced[row][col]=1;
-                index++;
-                if(row==0 || tableTraced[row-1][col]!=0){
+            else if(now%4==up){
+                if(tableTraced[row-1][col]!=0){
                     col--; // move left
-                    now="left";
+                    now++;
                 }
                 else row--;
             }
-            else if(now="left"){
-                printf(input[index]);
-                table[row][col]=input[index];
-                tableTraced[row][col]=1;
-                index++;
-                if(col==0 || tableTraced[row][col-1]!=0){
+            else if(now%4==left){
+                if(tableTraced[row][col-1]!=0){
                     row++; // move down
-                    now="down";
+                    now++;
                 }
                 else col--;
             }
 
         }
-        for(i=0;i<n;i++){
-            for(j=0;j<n;j++){
+
+        int stop=0;
+ /*       for(j=n;j>=1;j--){
+            if(table[n][j]==' ')
+                stop=j;
+            else
+                break;
+        }
+        for(i=1;i<=n;i++){
+            for(j=1;j<=n;j++){
+                if(i==n&& j==stop){
+                    break;
+                }
                 printf("[%c]", table[i][j]);
             }
             printf("\n");
         }
+*/
 
+        for(i = 1; i <= n; i++){
+            for(j = 1; j <= n; j++){
+                if(table[i][j] != ' ')
+                    stop = (i-1)*n+(j-1);
+            }
+        }
 
-
+        int len_index = 0;
+        for(i = 1; i <= n; i++){
+            for(j = 1; j <= n; j++){
+                printf("%c", table[i][j]);
+                len_index++;
+                if(len_index > stop)
+                    break;
+            }
+            //printf("\n");
+        }
+        printf("\n");
 
     }
 
